4168d920c18e325c029f7cca2e074ce4ca60a291
Rachid Bouhouch <rbouhouch@ribatis.com>
2024-12-31 10:00:27 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Weather App with React, Tanstack Query, Shadcn UI, Recharts, Tailwind, Typescript 🔥🔥
+
+### Make sure to create a `.env` file with following variables -
+```
+VITE_OPENWEATHER_API_KEY=
+```
---
+++ src/components/layout.tsx
@@ -0,0 +1,13 @@
+import type { PropsWithChildren } from "react";
+import { Header } from "./header";
+
+export function Layout({ children }: PropsWithChildren) {
+  return (
+    <div className=" bg-gradient-to-br from-background to-muted">
+      <Header />
+      <main className="min-h-screen container mx-auto px-4 py-8">
+        {children}
+      </main>
+    </div>
+  );
+}
